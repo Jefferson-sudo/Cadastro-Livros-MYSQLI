b8607fbaf970b68a6e682d0cbf7e66b0467f4aa9
Jefferson Mateus <jeffersonmateus64@gmail.com>
2020-12-01 15:21:45 -0300

    Estavel ate aqui

--- portoseguro/cadastro_livro/nova_editora.php
+++ portoseguro/cadastro_livro/nova_editora.php
@@ -1,1 +1,33 @@
+<?php
+    //Criando conexão com o banco de dados
+    $host     ="localhost";
+    $user     ="root";
+    $password ="";
+    $database ="cadastro_livro";
+    
+    $conexao = @mysqli_connect($host, $user, $password, $database);
+   mysqli_set_charset($conexao, 'utf8') or die(mysqli_error($conexao));
 
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curso mysqli Cadastro de Livro</title>
+    </head>
+    <body>
+        <h1 align="center">Nome da editora</h1>
+        <hr>
+        <br>
+        <form method="post">
+            <tr>
+                Editora: <td><input type="text" name="txt_editora"></td>
+                <td><input type="hidden" name="enviado" value="ok"></td>
+                <td><input type="submit" value="Cadastrar"></td>
+            </tr>
+        </form>
+    </body>
+</html>
