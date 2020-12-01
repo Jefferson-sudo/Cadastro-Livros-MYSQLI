c80809273728f5a28213b91af78d66d01a0dc71a
Jefferson Mateus <jeffersonmateus64@gmail.com>
2020-12-01 11:18:59 -0300

    01 ok

--- portoseguro/cadastro_livro/index.php
+++ portoseguro/cadastro_livro/index.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso mysqli Cadastro de Livro</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curso mysqli Cadastro de Livro</title>
 </head>
 <body>
-    <?php
-        //Code the project
-    ?>
+     <h1 align="center">Sistema de Cadastro de Livros</h1>
+     <hr>
+        <ul>
+             <li><a href="nova_editora.php"> Nova editora</a></li>
+             <li><a href="lista_editora.php">Lista de editoras</a></li>
+             <li><a href="busca_editora.php">Consuta editoras</a></li>
+        
+             <li><a href="lista_livro.php">Lista de Livros</a></li>
+             <li><a href="novo_livro.php">Novo Livro</a></li>
+             <li><a href="buca_livro.php">Consulta Livro</a></li>
+        </ul>
+     </hr>
 </body>
 </html>
 
